4cea02b5447388a1141d2073031e0ad84180acb0
teguh-budi2002 <budilaksono1102@gmail.com>
2023-05-03 22:34:25 +0700

    [UPDATE] Adding Config For Digiflazz & Midtrans

---
+++ config/digiflazz.php
@@ -0,0 +1,6 @@
+<?php
+
+return [
+  'username' => env('USERNAME_DIGIFLAZZ'),
+  'key' => env('DIGIFLAZZ_KEY')
+];
---
+++ config/midtrans.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+  'MIDTRANS_API_URL' => env('MIDTRANS_API_URL'),
+  'MIDTRANS_CLIENT_KEY' => env('MIDTRANS_CLIENT_KEY'),
+  'MIDTRANS_SERVER_KEY' => env("MIDTRANS_SERVER_KEY"),
+  'isProd' => env('APP_ENV')
+];
