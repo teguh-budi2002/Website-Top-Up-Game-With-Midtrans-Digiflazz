2a454c79418b663455b2912e2bd14106de52a027
teguh-budi2002 <budilaksono1102@gmail.com>
2023-06-24 14:11:01 +0700

    [UPDATE] Apply Animation Notification

--- resources/views/components/bell-notification.blade.php
+++ resources/views/components/bell-notification.blade.php
@@ -23,7 +23,7 @@
             <path stroke-linecap="round" stroke-linejoin="round"
                 d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
         </svg>
-        <div class="notif__dropdown absolute top-14 -left-96 bg-white w-[430px] h-auto p-1 rounded-md z-50" x-show="isNotifOpen">
+        <div class="notif__dropdown absolute top-14 -left-96 bg-white w-[430px] h-auto p-1 rounded-md z-50" x-show="isNotifOpen" x-transition.duration.400ms>
             <div class="header__notif flex justify-between items-center px-4 text-slate-600 mt-3">
                 <p class="uppercase font-bold">Notifikasi</p>
                 <div @click.stop="isNotifOpen = false">
