d57062cbdfba3cfa00ea44492f52ebf6c1f03a98
teguh-budi2002 <budilaksono1102@gmail.com>
2023-08-17 19:31:06 +0700

    [UPDATE] {BACKEND} Fix Bug Old Multiple Img Not Deleted When Update Image

--- app/Http/Controllers/Layout/LayoutController.php
+++ app/Http/Controllers/Layout/LayoutController.php
@@ -5,7 +5,9 @@ namespace App\Http\Controllers\Layout;
 use App\Models\NavLayout;
 use App\Models\BannerLayout;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\File;
 use Illuminate\Support\Facades\Storage;
 
 class LayoutController extends Controller
@@ -61,52 +63,70 @@ class LayoutController extends Controller
      * -------------------------------------
      */
     public function editMainBannerLayout(Request $request) {
-        $validation = $request->validate([
-            'images' => 'array|min:1|required',
-            'images.*' => 'image|max:2048',
-        ], [
-            'images.required' => 'Input Image Cannot Be Null.', 
-            'images.*.image' => 'The Uploaded File Must be an Image.', 
-            'images.*.uploaded' => 'Max File Size is 2MB.',
-        ]);
         
-        $file_images = $request->images;
-        $uploadedImages = [];
-        
-        foreach ($file_images as $img) {
-            $filename = $img->getClientOriginalName();
-            $extension = $img->getClientOriginalExtension();
-            $path = "page/banner/";
-            $this->deleteOldImages($path, $filename);
+        DB::beginTransaction();
+        try {
+            $validation = $request->validate([
+                'images' => 'array|min:1|required',
+                'images.*' => 'image|max:2048',
+            ], [
+                'images.required' => 'Input Image Cannot Be Null.', 
+                'images.*.image' => 'The Uploaded File Must be an Image.', 
+                'images.*.uploaded' => 'Max File Size is 2MB.',
+            ]);
             
-            if (!in_array($extension, ['webp', 'png'])) {
-                return redirect()->back()->with('extension-error', 'Allowed Extension For Image (webp, png)');
+            $file_images = $request->images;
+            $uploadedImages = [];
+                
+            foreach ($file_images as $img) {
+                $filename = $img->getClientOriginalName();
+                $extension = $img->getClientOriginalExtension();
+                $path = "page/banner/";
+
+                if (!in_array($extension, ['webp', 'png'])) {
+                    return redirect()->back()->with('extension-error', 'Allowed Extension For Image (webp, png)');
+                }
+
+                $uploadedImages[] = $path . $filename;
+                $putImgIntoStorage = Storage::putFileAs('/public/page/banner/', $img, $filename);
             }
+            $this->deleteOldImages($uploadedImages);
+    
+            if (BannerLayout::count() >= 1) {
+                $oldestBanner = BannerLayout::select("id", "img_url")->oldest()->first(); 
+                $oldestBanner->delete();
+            }
+    
+            $insertedBanner = BannerLayout::create([
+                'img_url' => $uploadedImages
+            ]);
 
-            $putImgIntoStorage = Storage::putFileAs('/public/page/banner/', $img, $filename);
-            $uploadedImages[] = $path . $filename;
+            // if (!is_null($insertedBanner)) {
+                
+            // }
+
+            DB::commit();
+            return redirect()->back()->with('edit_banner_layout_success', 'Edit Banner Layout Successfully');
+        } catch (\Throwable $th) {
+            DB::rollback();
+            return redirect()->back()->with('error_server', 'Error From ServerSide. Please Contact Admin To Fix This!');
         }
-
-        if (BannerLayout::count() >= 1) {
-            $oldestBanner = BannerLayout::select("id", "img_url")->oldest()->first(); 
-            $oldestBanner->delete();
-        }
-
-        BannerLayout::create([
-            'img_url' => $uploadedImages
-        ]);
-        return redirect()->back()->with('edit_banner_layout_success', 'Edit Banner Layout Sukses');
     }
 
-    protected function deleteOldImages($path, $filename) {
+    protected function deleteOldImages($filenames) {
         $banners = BannerLayout::select("id", "img_url")->first();
         if (!is_null($banners)) {
-            foreach ($banners->img_url as $banner) {
-                 //Check If Any Old BG Image
-                if ($banner != $path . $filename) {
-                    // Delete Old Image from Storage
-                    $deleteOldImg = Storage::disk('public')->delete($banner);
-                }
+            foreach ($banners->img_url as $oldBanner) {
+                $imgUrl = Storage::url($oldBanner);
+                $localPath = public_path(trim($imgUrl, '/'));
+
+                //Check If Any Old BG Image
+                if (file_exists($localPath)) {
+                    if (!in_array($oldBanner, $filenames)) {
+                        // Delete Old Image from Storage
+                        unlink($localPath);
+                    }
+                }  
             }
         }
 	}
